eebe956a048b1217abc173cee543d5a00b49f510
김희수 <heesu01@gimhuisuui-noteubug.local>
2024-06-19 00:15:21 +0900

    feat: InputFilled 컴포넌트 구현

---
+++ src/components/InputFilled.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { Theme } from "../styles/Theme";
+
+const InputFilled = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const clearInput = () => {
+    setInputValue("");
+  };
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <InputContainer>
+        <Filled
+          placeholder="아이디"
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        {inputValue && <ClearButton onClick={clearInput}>X</ClearButton>}
+      </InputContainer>
+    </ThemeProvider>
+  );
+};
+
+const InputContainer = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const Filled = styled.input`
+  width: 484px;
+  min-height: 36px;
+  border: 2px solid ${({ theme }) => theme.colors.gray};
+  border-radius: 25px;
+  ${({ theme }) => theme.fonts.default};
+  padding: 29px 27px;
+  gap: 10px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const ClearButton = styled.button`
+  width: 32px;
+  height: 32px;
+  position: absolute;
+  right: 28.5px;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: ${({ theme }) => theme.colors.gray};
+  color: #fff;
+  border-radius: 50px;
+  font-size: 20px;
+`;
+
+export default InputFilled;

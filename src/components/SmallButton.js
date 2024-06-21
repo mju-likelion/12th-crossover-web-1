7294c36a7ddcabd6b94886f953bc28924f323516
김희수 <heesu01@gimhuisuui-noteubug.local>
2024-06-21 13:41:23 +0900

    fix: 깃충돌로 다시생성

--- src/components/SmallButton.js
+++ src/components/SmallButton.js
@@ -2,22 +2,21 @@ import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { Theme } from "../styles/Theme";
 
-const SmallButton = () => {
+const SmallButton = ({ buttonText, isEnabled }) => {
   return (
     <ThemeProvider theme={Theme}>
       <Container>
-        <Button>작성하기</Button>
+        <Button isEnabled={isEnabled}>{buttonText}</Button>
       </Container>
     </ThemeProvider>
   );
 };
-const Container = styled.div`
-  margin: 20px;
-`;
+const Container = styled.div``;
 const Button = styled.button`
   width: 233px;
-  height: 78px;
-  background-color: ${({ theme }) => theme.colors.blue1};
+  height: 70px;
+  background-color: ${({ theme, isEnabled }) =>
+    isEnabled ? theme.colors.blue2 : theme.colors.blue1};
   color: #fff;
   text-align: center;
   border-radius: 25px;

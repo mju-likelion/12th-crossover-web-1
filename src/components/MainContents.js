6fe28f6f5a4f01a2baa9cf224dff2bb2a913a966
jogpfls <joerin4177@gmail.com>
2024-06-28 22:28:10 +0900

    api: 전체 게시물 조회

--- src/components/MainContents.js
+++ src/components/MainContents.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
-//import GrayProfile from "../assets/images/gray_profile.svg";
+import React from "react";
+import styled from "styled-components";
 import BlueProfile from "../assets/images/profile.svg";
 
-const MainContents = () => {
+const MainContents = ({ title, content, time }) => {
+    const formatTime = (dateString) => {
+        const date = new Date(dateString);
+        const hours = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        return `${hours}:${minutes}`;
+    };
     return (
         <>
         <WrapperBox>
             <Box>
-                <ProfileBox>
-                    <TextBox>
-                        <ProfileImg src={BlueProfile} alt="profileImg"/>
-                        <Right>
-                            <Title>제목: text</Title>
-                            <ContentBox>
-                                <Contents>text</Contents>
-                            </ContentBox>
-                        </Right>
-                    </TextBox>
-                </ProfileBox>
-                <Time>6:30</Time>
+            <ProfileBox>
+                <TextBox>
+                <ProfileImg src={BlueProfile} alt="profileImg" />
+                <Right>
+                    <Title>제목: {title}</Title>
+                    <ContentBox>
+                    <Contents>{content}</Contents>
+                    </ContentBox>
+                </Right>
+                </TextBox>
+            </ProfileBox>
+            <Time>{formatTime(time)}</Time>
             </Box>
         </WrapperBox>
         </>
@@ -75,8 +80,7 @@ const Right = styled.div`
     align-items: end;
     flex-direction: column;
     justify-content: space-between;
-    `;
-
+`;
 
 const Title = styled.p`
     width: 598px;
@@ -86,7 +90,7 @@ const Title = styled.p`
 `;
 
 const ContentBox = styled.p`
-    margin-top: 13px;
+  margin-top: 13px;
     width: 598px;
     height: 198px;
     border-radius: 25px;
